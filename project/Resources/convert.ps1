73ec319bec1e3b49546f8b38144a7e8d5f516b46
IshikawaTakeshi <K023G0065@g.neec.ac.jp>
2025-10-20 16:19:40 +0900

    Resorceファイル内のテクスチャをpowercellを使って一括変換できるようにした

--- project/Resources/convert.ps1
+++ project/Resources/convert.ps1
@@ -1,2 +1,6 @@
-Start-Process -FilePath TextureConverter.exe -ArgumentList Exit8.png -Wait
+$files = Get-Item *.png
+
+foreach($f in $files){
+    Start-Process -FilePath TextureConverter.exe -ArgumentList $f -Wait
+}
 pause
